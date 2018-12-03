6fa7da0eceb2fa3eccd53d77a5ae6c178940c0fa
Igor Cemim <igor@cemim.com.br>
2018-12-02 22:12:42 -0200

    Adicionada validação para não permitir apagar cliente caso existam propostas vinculadas

--- src/main/java/br/com/cemim/igor/dao/ClienteDAO.java
+++ src/main/java/br/com/cemim/igor/dao/ClienteDAO.java
@@ -1,8 +1,11 @@
 package br.com.cemim.igor.dao;
 
 import br.com.cemim.igor.entidade.Cliente;
+import br.com.cemim.igor.exception.ErroSistema;
 
 import java.io.Serializable;
+
+import javax.persistence.Query;
 
 public class ClienteDAO extends CrudDAO<Cliente> implements Serializable {
 
@@ -13,5 +16,19 @@ public class ClienteDAO extends CrudDAO<Cliente> implements Serializable {
 
     public Class<Cliente> getEntidade() {
         return Cliente.class;
+    }
+
+    private long quantidadePropostasVinculadas(Cliente cliente) {
+        Query query = getEm()
+            .createQuery("select count(*) from Proposta p where p.cliente.id = :idCliente")
+            .setParameter("idCliente", cliente.getId());
+        return (long) query.getSingleResult();
+    }
+
+    public void apagar(Cliente elemento) throws ErroSistema {
+        if (quantidadePropostasVinculadas(elemento) > 0) {
+            throw new ErroSistema("Não é possível apagar o cliente. Existem propostas vinculadas.");
+        }
+        super.apagar(elemento);
     }
 }
