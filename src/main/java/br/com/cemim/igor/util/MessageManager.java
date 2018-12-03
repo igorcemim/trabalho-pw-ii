9070b2fe5178f2d8c784eb6b191922acec788bde
Igor Cemim <igor@cemim.com.br>
2018-12-02 22:15:44 -0200

    Corrigido log de erros

--- src/main/java/br/com/cemim/igor/util/MessageManager.java
+++ src/main/java/br/com/cemim/igor/util/MessageManager.java
@@ -1,21 +1,20 @@
 
 package br.com.cemim.igor.util;
 
-import br.com.cemim.igor.controller.CrudBean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 public class MessageManager {
-    
+
     public void adicionarInfo(String mensagem) {
         FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, null);
         FacesContext.getCurrentInstance().addMessage(null, fm);
     }
 
     public void adicionarErro(String mensagem) {
-        Logger.getLogger(CrudBean.class.getName()).log(Level.SEVERE, null, mensagem);
+        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, mensagem);
         FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, null);
         FacesContext.getCurrentInstance().addMessage(null, fm);
     }
