07921743c2ebe12ecb170cebabb4439aa0840397
Igor Cemim <igor@cemim.com.br>
2018-12-03 02:38:10 -0200

    Adicionado bean para login e depedência Apache Commons Codec

---
+++ src/main/java/br/com/cemim/igor/controller/LoginBean.java
@@ -0,0 +1,16 @@
+package br.com.cemim.igor.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean implements Serializable {
+
+    public void logar() {
+        // @todo Implementar.
+    }
+
+}
