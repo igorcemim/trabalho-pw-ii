1b5923a3ed3afc89b6c68790b4d443e3e3c28ac5
Igor Cemim <igor@cemim.com.br>
2018-12-03 13:03:06 -0200

    Adicionado utilitário para gerenciamento de sessão

---
+++ src/main/java/br/com/cemim/igor/util/SessionManager.java
@@ -0,0 +1,40 @@
+package br.com.cemim.igor.util;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+public class SessionManager {
+
+    private static SessionManager instance;
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+
+        return instance;
+    }
+
+    private SessionManager() {
+    }
+
+    private ExternalContext currentExternalContext() {
+        if (FacesContext.getCurrentInstance() == null) {
+            throw new RuntimeException("O FacesContext não pode ser chamado fora de uma requisição HTTP");
+        } else {
+            return FacesContext.getCurrentInstance().getExternalContext();
+        }
+    }
+
+    public void invalidateSession() {
+        currentExternalContext().invalidateSession();
+    }
+
+    public Object getAttribute(String nome) {
+        return currentExternalContext().getSessionMap().get(nome);
+    }
+
+    public void setAttribute(String nome, Object valor) {
+        currentExternalContext().getSessionMap().put(nome, valor);
+    }
+}
